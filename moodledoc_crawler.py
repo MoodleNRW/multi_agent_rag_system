1c1f35a75e899d1e8ce6ccab350ebfc65c3da208
Laur0r <laura-troost@t-online.de>
2024-09-04 14:31:00 +0200

    Add summary to DB

--- moodledoc_crawler.py
+++ moodledoc_crawler.py
@@ -3,13 +3,16 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
 import sys
 import os
 import time
 import threading
 import weaviate
-from langchain_community.vectorstores import Weaviate
-import weaviate.classes as wvc
+from langchain_openai import ChatOpenAI 
 from datetime import datetime
 import dotenv
 dotenv.load_dotenv()
@@ -40,6 +43,17 @@ if not weaviate_client.schema.exists("Content"):
 if not weaviate_client.schema.exists("Content_chunk"):
     class_obj = {
         "class": "Content_chunk",
+        "vectorizer": "text2vec-openai",  # If set to "none" you must always provide vectors yourself. Could be any other "text2vec-*" also.
+        "moduleConfig": {
+            "text2vec-openai": {},
+            "generative-openai": {}  # Ensure the `generative-openai` module is used for generative queries
+        }
+    }
+    weaviate_client.schema.create_class(class_obj)
+
+if not weaviate_client.schema.exists("Content_summary"):
+    class_obj = {
+        "class": "Content_summary",
         "vectorizer": "text2vec-openai",  # If set to "none" you must always provide vectors yourself. Could be any other "text2vec-*" also.
         "moduleConfig": {
             "text2vec-openai": {},
@@ -166,6 +180,7 @@ def scrape_website(url, visited=None, max_workers=10, depth=10):
                         "date": datetime.now().isoformat(),
                     },
                 )
+
             # Chunk import 
             text_splitter = RecursiveCharacterTextSplitter(
                 # Set a really small chunk size, just to show.
@@ -217,6 +232,46 @@ def scrape_website(url, visited=None, max_workers=10, depth=10):
                         },
                     )
                 i = i + 1
+
+            # Summary
+            docs = [Document(page_content=text)]
+            if not text:
+                break
+            llm = ChatOpenAI(temperature=0, model_name="gpt-4o", max_tokens=2000)
+            # Define prompt
+            prompt_template = "Summarize this content: {context}"
+            prompt = ChatPromptTemplate.from_template(prompt_template)
+            stuff_chain = create_stuff_documents_chain(llm, prompt)
+            summary = stuff_chain.invoke({"context": docs})
+            
+            where_filter = {
+                "path": ["url"],
+                "operator": "Equal",
+                "valueString": url
+            }
+            # Check if url already exists
+            obj = weaviate_client.query.get("Content_summary","url").with_where(where_filter).with_additional('id').do()
+            properties = {
+                "url": url,
+                "content_summary": summary,
+                "date": datetime.now().isoformat(),
+            }
+            # if not create new object
+            if 'errors' in obj or not obj['data']['Get']['Content_summary']:
+                batch.add_data_object(
+                    data_object=properties,
+                    class_name="Content_summary"
+                )
+            # else update existing
+            else:
+                weaviate_client.data_object.update(
+                    uuid=obj['data']['Get']['Content_summary'][0]['_additional']['id'],
+                    class_name="Content_summary",
+                    data_object={
+                        "content_summary": text,
+                        "date": datetime.now().isoformat(),
+                    },
+                )
 
     return ''
     #return '\n'.join([f"# Page: {url}\n\n{text}\n" for url, text in results])
